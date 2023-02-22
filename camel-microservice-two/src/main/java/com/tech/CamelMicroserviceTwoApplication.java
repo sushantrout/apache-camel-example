711376616013a460c95db0d4968a924cfeaf02bc
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2023-02-22 17:35:10 +0530

    IC

---
+++ camel-microservice-two/src/main/java/com/tech/CamelMicroserviceTwoApplication.java
@@ -0,0 +1,13 @@
+package com.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMicroserviceTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMicroserviceTwoApplication.class, args);
+	}
+
+}
